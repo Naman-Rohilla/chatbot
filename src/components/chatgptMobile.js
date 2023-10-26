e73ac83fb58537f75f861d8097e5f623f945ba1c
NAMAN ROHILLA <71631876+Naman-Rohilla@users.noreply.github.com>
2023-10-26 09:55:48 +0530

    Update chatgptMobile.js

--- src/components/chatgptMobile.js
+++ src/components/chatgptMobile.js
@@ -39,7 +39,7 @@ export default function ChatgptMobile() {
       setQuery("");
       setCurrentchatarray([...currentchatarray, query]);
       try {
-        let finalstr = "Your name is Kush the most intelligent guy, If someone greets you you also greets him. Now, You can use this paragraph for refernce:";
+        let finalstr = "Your name is Kush the most intelligent guy, If someone greets you you also greets the same. Now, You can use this paragraph for refernce:";
         if (query.length < 300) {
           for (var i = 0; i < currentchatarray.length; i++) {
             finalstr = finalstr + currentchatarray[i] + "\n";
